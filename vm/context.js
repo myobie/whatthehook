3e1f69c66d47dd9f938a0cca63f9e37336480734
Nathan Herald <me@nathanherald.com>
2018-05-05 11:16:08 -0700

    I think I can now execute some code and get the result

--- vm/context.js
+++ vm/context.js
@@ -1,55 +1,34 @@
 const vm = require('vm')
 const { inspect } = require('util')
 
-function getMethods (obj) {
-  var res = []
-  for (var m in obj) {
-    if (typeof obj[m] === 'function') {
-      res.push(m)
-    }
-  }
-  return res
-}
-
-const sandbox = {
-  dir: function (what) {
-    console.dir(what)
-    console.log(inspect(what))
-    console.log(what.name)
-    console.dir(Object.keys(what))
-    console.dir(Object.keys(what.prototype))
-    console.dir(getMethods(what))
-    console.dir(getMethods(what.prototype))
-  },
-  fetch: function () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        // reject(new Error('request failed'))
-        resolve({})
-      }, 1000)
-    })
-  }
-}
-
-const setupStateCode = `
+const setupCode = `
   'use strict'
 
   var global = this
 
-  Object.setPrototypeOf(global, Object.prototype)
-
   Object.defineProperties(global, {
-    constructor: { value: Object },
     global: {value: global},
     GLOBAL: {value: global},
     root: {value: global},
     isVM: {value: true}
   })
 
-  const sandboxKeys = [${Object.keys(sandbox).map(n => `'${n}'`).join(',')}]
+  contextify(global)
 
-  sandboxKeys.forEach(name => {
-    let o = this[name]
+  for (let i in global._imports) {
+    global[i] = contextify(global._imports[i])
+  }
+
+  delete global._imports
+
+  var state = {}
+
+  var result = undefined
+  var error = undefined
+  var currentArguments = []
+  var module = { exports: undefined }
+
+  function contextify (o) {
     let klass
 
     if (typeof o === 'function') {
@@ -58,85 +37,114 @@ const setupStateCode = `
       klass = Object
     }
 
+    Object.setPrototypeOf(o, klass.prototype)
+
     Object.defineProperties(o, {
-      constructor: { value: klass },
-      __proto__: { value: klass.prototype },
-      prototype: { value: klass.prototype }
+      constructor: { value: klass }
     })
-  })
 
-  var state = {}
-  var module = { exports: undefined }
-  var result = undefined
-  var error = undefined
-  var currentArguments = []
-`
-
-let setupStateScript
-try {
-  setupStateScript = new vm.Script(setupStateCode, { filename: 'user.js', timeout: 5000, displayErrors: true })
-} catch (e) {
-  console.error('code compilation for setup state code failed', e)
-  process.exit(1)
-}
-
-const userSuppliedCode = `
-  'use strict'
-
-  // this.constructor.constructor('return process')().exit()
-  // fetch.constructor.constructor('return process')().exit()
-
-  module.exports = async function () {
-    // while (true) { 1 }
-    return await fetch('https://github.com/myobie.keys')
+    return o
   }
 `
-
-let userSuppliedScript
-try {
-  userSuppliedScript = new vm.Script(userSuppliedCode, { filename: 'user.js', timeout: 5000, displayErrors: true })
-} catch (e) {
-  console.error('code compilation for user supplied code failed', e)
-  process.exit(1)
-}
 
 const postCode = `
   'use strict'
 
-  Promise.resolve(module.exports.apply(null, currentArguments))
-    .then(r => { result = JSON.stringify(r) })
-    .catch(e => { error = e })
-`
-
-let postScript
-try {
-  postScript = new vm.Script(postCode, { filename: 'user.js', timeout: 5000, displayErrors: true })
-} catch (e) {
-  console.error('code compilation for post code failed', e)
-  process.exit(1)
-}
-
-const context = vm.createContext(sandbox)
-
-module.exports = async function runTestInContext () {
+  // TODO: add a try/catch in there
   try {
-    Promise.resolve({})
-      .then(() => {
-        Promise.resolve(setupStateScript.runInContext(context, { filename: 'user.js', timeout: 5000, displayErrors: true }))
-      })
-      .then(() => {
-        Promise.resolve(userSuppliedScript.runInContext(context, { filename: 'user.js', timeout: 5000, displayErrors: true }))
-      })
-      .then(() => {
-        return Promise.resolve(postScript.runInContext(context, { filename: 'user.js', timeout: 5000, displayErrors: true }))
-      })
-      .then(() => {
-        console.dir(sandbox)
+    Promise.resolve(request(...currentArguments))
+      .then(r => {
+        try {
+          finalResult(null, JSON.stringify(r))
+        } catch (e) {
+          log('result returned from request is invalid')
+        }
       })
       .catch(e => {
-        console.error('execution error in async', e)
+        try {
+          finalResult(e, null)
+        } catch (wow) {
+          log('An unrecoverable error occured')
+          log(wow)
+        }
       })
   } catch (e) {
-    console.error('execution error', e)
+    finalResult(e, null)
+  }
+`
+
+module.exports = class Context {
+  constructor (code, resultCallback) {
+    this._code = code
+
+    this._imports = {
+      log: this.log,
+      fetch: this.fetch,
+
+      // FIXME: this means we can only ever do one thing at a time
+      //        instead we should have a UUID for every request and use
+      //        that to corollate results
+      finalResult: function (err, resultString) {
+        resultCallback(err, resultString)
+      }
+    }
+
+    this.timeout = 5000
+    this._sandbox = {_imports: this._imports}
+    this._context = vm.createContext(this._sandbox)
+  }
+
+  log (what) {
+    console.log(inspect(what))
+  }
+
+  fetch () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // reject(new Error('request failed'))
+        resolve({status: 500, body: ''})
+      }, 1000)
+    })
+  }
+
+  prepare () {
+    const setup = this._compile(setupCode, 'setup.js')
+    this._run(setup)
+
+    const user = this._compile(this._code, 'user.js')
+    this._run(user)
+  }
+
+  execute (...args) {
+    try {
+      this._run(this._compile(`
+        'use strict'
+        currentArguments = ${JSON.stringify(args)}
+      `), 'arguments.js')
+
+      this._run(this._compile(postCode, 'execute.js'))
+    } catch (e) {
+    }
+  }
+
+  _compile (code, filename) {
+    console.log(`compiling code:
+${code}
+`)
+    try {
+      return new vm.Script(code, { filename, timeout: this.timeout, displayErrors: true })
+    } catch (e) {
+      console.error('code compilation failed', e)
+      throw e
+    }
+  }
+
+  _run (script, filename) {
+    try {
+      return script.runInContext(this._context, { filename, timeout: this.timeout, displayErrors: true })
+    } catch (e) {
+      console.error('execution error', e)
+      throw e
+    }
   }
 }
--- vm/index.js
+++ vm/index.js
@@ -1,3 +1,40 @@
-const run = require('./context')
+const Context = require('./context')
 
-run()
+const code = `
+  'use strict'
+
+  try {
+    this.constructor.constructor('return process')().exit()
+  } catch (e) {
+    log('this hack failed')
+    log(e)
+  }
+
+  try {
+    fetch.constructor.constructor('return process')().exit()
+  } catch (e) {
+    log('fetch hack failed')
+    log(e)
+  }
+
+  async function request ({ user }) {
+    // while (true) { 1 }
+    log('fetching ' + user)
+    return await fetch('https://github.com/' + user + '.keys')
+  }
+`
+
+const c = new Context(code, (err, result) => {
+  if (err) {
+    console.error('execution error', err)
+  } else {
+    console.log('result', result)
+  }
+})
+
+// invoke once
+c.prepare()
+c.execute({ user: 'myobie' })
+
+// invoke again
+c.execute({ user: 'waht' })

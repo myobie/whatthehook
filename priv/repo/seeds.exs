344efdd18704798f3e7a62c933743482b73b7fc0
Nathan Herald <me@nathanherald.com>
2018-05-31 10:51:58 +0200

    Add another hook for testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -4,6 +4,18 @@
 function request (req) {
   const counter = req.state.counter || 0;
 
+  return { state: { counter: counter + 1 } }
+}
+"""
+}
+|> WTH.Repo.insert!()
+
+%WTH.Webhooks.Hook{
+  endpoint: "example.com",
+  code: """
+function request (req) {
+  const counter = req.state.counter || 0;
+
   return fetch('http://google.com')
     .then(res => {
       res.state = { counter: counter + 1 }
